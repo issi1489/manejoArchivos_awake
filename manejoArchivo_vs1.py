a482fddd96a312e03cb21f07b666bf0d1b08161c
issi1489 <issi1489@gmail.com>
2020-11-27 23:18:59 -0300

    archivos base para ejercicio de manejo de archivos

---
+++ manejoArchivo_vs1.py
@@ -0,0 +1,21 @@
+'''
+Obtenga los 4 archivos de datos en formato txt provistos para este
+ejercicio.
+Ubique los datos en un directorio data/ ubicado dentro del directorio
+que creó para la realización de este ejercicio.
+Los archivos son:
+• datos1.xml: En formato XML.
+• datos2.json: En formato JSON.
+• datos3.csv: En formato CSV.
+• datos4.txt: En formato separado por espacios.
+Los archivos contienen datos con los mismos tipos de variables, pero
+que han sido almacenadas en distintos formatos.
+Se solicita:
+1. Organice su grupo de desarrollo con distintos roles para la creación
+del código y para el manejo de un repositorio del ejercicio llamado
+archivos_multiformato.
+'''
+
+#imports
+
+#code
