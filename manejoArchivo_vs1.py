7497bbb800821f2209a85aaacd64d719aaa98e67
issi1489 <issi1489@gmail.com>
2020-11-27 23:25:34 -0300

    Version casi final de la prueba

--- manejoArchivo_vs1.py
+++ manejoArchivo_vs1.py
@@ -14,6 +14,30 @@ Se solicita:
 1. Organice su grupo de desarrollo con distintos roles para la creación
 del código y para el manejo de un repositorio del ejercicio llamado
 archivos_multiformato.
+2. Acuerde el esquema de ramas para el desarrollo convergente.
+3. Cargar los datos provenientes desde los distintos archivos a las
+variables: datos1, datos2, datos3 y datos4.
+4. Imprimir en pantalla los datos tal como han sido cargados desde los
+archivos.
+5. Unificar todos los datos en una sola estructura llamada datos_total.
+Para esto deberá tener cuidado de no mezclar nombres de columnas
+entre los distintos conjuntos de datos provenientes de diferentes
+archivos.
+6. Mostrar los datos en pantalla con aspecto de tabla con títulos de
+columnas.
+7. Realizar una copia de los datos a otra variable que contenga solo las
+primeras 4 columnas de los datos.
+8. Cree 3 nuevas columnas, a la derecha de las 4 existentes, llamadas
+‘max’, ‘min’, ‘promedio’.
+9. Llene esas columnas con los valores máximos, mínimos y promedio
+de cada fila, respectivamente.
+10. Calcule y muestre en pantalla el valor máximo, mínimo y
+promedio de cada una de las primeras 4 columnas.
+11. Muestre en pantalla la tabla completa actualizada.
+12. Guarde los datos resultantes en 4 archivos diferentes, con
+formatos XML, JSON, CSV y Separado por expacios.
+13. Deje sus resultados en el repositorio GitHub del ejercicio.
+
 '''
 
 #imports
